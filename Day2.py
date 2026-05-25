f844be90fc98a10c5d0733c341a2f2e809936cb3
avej mulla <avejmulla786786@gmail.com>
2026-05-25 23:47:13 +0530

    Day 2

---
+++ Day2.py
@@ -0,0 +1,17 @@
+a = 22.2
+b = 40 
+c = "Avej"
+
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+list = [8,2,4,7,5,[-4,5]["apple","banana"]]
+print(list)
+
+tuple(("parrot","sparrow"),("lion,tiger"))
+print(tuple)
+
+dic1 = {"name":avej,"age":20,"canvote":True}
+print(dic1)
