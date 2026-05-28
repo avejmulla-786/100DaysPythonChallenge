88184624c1e9d3b6e35734efa86917ff23aba649
avej mulla <avejmulla786786@gmail.com>
2026-05-28 16:16:45 +0530

    Added Day4

---
+++ Day4.py
@@ -0,0 +1,21 @@
+name = "Avej"
+friend = "Mohsin"
+anotherfriend = "Faizan"
+
+apple = '''Apple Inc. is an American multinational technology company headquartered in Cupertino, California, in Silicon Valley, and known for consumer electronics, software and online services.
+Wikipedia
+Designated CEO: John Ternus (1 Sept 2026–)
+Customer service: 000 800 100 9009
+Founders: Steve Jobs, Steve Wozniak, Ronald Wayne
+Founded: 1 April 1976, Los Altos, California, United States
+Headquarters: Cupertino, California, United States
+COO: Sabih Khan
+CEO: Tim Cook (24 Aug 2011–1 Sept 2026)'''
+
+print("hello" + name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
