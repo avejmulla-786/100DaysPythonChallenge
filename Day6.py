0838696086d2ac37f9f67b8b3a6d132ed0f0fd16
avej mulla <avejmulla786786@gmail.com>
2026-05-31 22:29:24 +0530

    center

--- Day6.py
+++ Day6.py
@@ -7,3 +7,10 @@ print(a.rstrip(""))
 print(a.replace("Avej","Adil"))
 print(a.split(" "))
 
+blogHeading = "introduction to js"
+print(blogHeading.capitalize)
+
+
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
