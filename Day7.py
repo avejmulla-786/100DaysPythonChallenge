1e11edd492128419adc9dfcb84b49ed6c2006791
avej mulla <avejmulla786786@gmail.com>
2026-06-01 23:52:03 +0530

    Day 7 - Budget Condition Program

--- Day7.py
+++ Day7.py
@@ -16,3 +16,20 @@ elif (num > 0):
 
 else:
     print("Number is zero")
+
+
+
+
+
+
+applePrice = 10
+budget = 200
+
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+
+elif (budget - applePrice > 70):
+    print("Its okay you can buy")
+
+else:
+    print("Alexa, do not add Apples to the cart.")    
