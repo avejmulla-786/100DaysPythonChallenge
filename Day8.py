2c623b7537b45cd4bc5fa5e0f53540955974c1df
avej mulla <avejmulla786786@gmail.com>
2026-06-02 23:35:11 +0530

    Day 8 - Good Morning Program

---
+++ Day8.py
@@ -0,0 +1,16 @@
+import time 
+#= int(input("Enter 1 :"))
+# = int(input("Enter 2 :"))
+# = int(input("Enter 3 :"))
+
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+
+timestamp = time.strftime('%H')
+print(timestamp)
+
+timestamp = time.strftime('%M')
+print(timestamp)
+
+timestamp = time.strftime('%S')
+print(timestamp)
