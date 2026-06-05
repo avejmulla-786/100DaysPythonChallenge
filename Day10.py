c6f444d42f985c7a8fdae4414176e068fb91845e
avej mulla <avejmulla786786@gmail.com>
2026-06-05 23:09:19 +0530

    Day 10 - For Loop With Colors

---
+++ Day10.py
@@ -0,0 +1,17 @@
+
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+
+for color in colors:
+    print(color)
+
+    for i in color:
+        print(i)
+
+print("Colors loop completed ✅")
+
+
+
+for k in range(1, 12, 3):
+    print(k)
+
