ff06368efcc87b98977e76b271570c092994e341
avej mulla <avejmulla786786@gmail.com>
2026-06-06 22:27:13 +0530

    Day 11 - While Loop Practice

---
+++ Day11.py
@@ -0,0 +1,15 @@
+i = int(input("Enter the no"))
+while(i<=55):
+     i = int(input("Enter the no"))
+     print(i)
+
+    
+print("Done with the loop")    
+
+
+count = -5 
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am in side the else")
