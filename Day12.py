0b8443ba899df2db598e99ce5b44c6467046a6d6
avej mulla <avejmulla786786@gmail.com>
2026-06-07 15:11:43 +0530

    Day 12 - Advanced Break Continue Loops

--- Day12.py
+++ Day12.py
@@ -6,3 +6,29 @@ for i in range(12):
    print("5 X" , i+1 ," = " , 5 * (i+1))
 
 print("exit the loop")   
+
+# Extra Loop
+
+for num in range(1, 6):
+   print("Number:", num)
+
+print("Second loop completed ")
+
+
+# Even and Odd Check
+
+for n in range(1, 11):
+
+   if(n % 2 == 0):
+      print(n, "is Even")
+
+   else:
+      print(n, "is Odd")
+
+print("Even Odd loop completed ")
+
+
+# Reverse Counting
+
+for x in range(10, 0, -1):
+   print("Countdown:", x)
