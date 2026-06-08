4b5bb04d2124ebbb125fa92192e3e4ee6ca5e4c7
avej mulla <avejmulla786786@gmail.com>
2026-06-08 23:59:34 +0530

    Day 13 - Functions in Python

---
+++ Day13.py
@@ -0,0 +1,36 @@
+def calculateGmean(a, b):
+    mean = (a * b) / (a + b)
+    print("Geometric Mean is:", mean)
+
+
+def isGreater(a, b):
+    if(a > b):
+        print("First number is greater")
+
+    else:
+        print("Second number is greater or equal")
+
+
+def isLesser(a, b):
+    pass
+
+
+a = 9
+b = 8
+
+isGreater(a, b)
+calculateGmean(a, b)
+
+
+# Extra Functions
+
+def addition(x, y):
+    print("Addition =", x + y)
+
+
+def subtraction(x, y):
+    print("Subtraction =", x - y)
+
+
+addition(a, b)
+subtraction(a, b)
