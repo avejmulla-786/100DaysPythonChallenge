94e41ae10668bf80b99e1b244edabb8cc31cab5e
avej mulla <avejmulla786786@gmail.com>
2026-06-09 14:08:38 +0530

    Day 13 - Average and Multiple Number Average Functions

--- Day14.py
+++ Day14.py
@@ -1,4 +1,12 @@
 def average(a,b):
-    print("The average is :",a+b/2)
+   print("The average is :",(a+b)/2)
     
 average(3,8)    
+
+def multiple_no_average(*numbers):
+    sum = 0
+    for i in numbers:
+       sum = sum + i
+    print("Total Average is:",sum/len(numbers))
+
+multiple_no_average(52,46,34,56,45,55)       
