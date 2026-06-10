0c0ce3d84b851436bf59a59641b3004d080172fe
avej mulla <avejmulla786786@gmail.com>
2026-06-10 20:08:45 +0530

    Day 15 - List Methods Practice

--- Day15.py
+++ Day15.py
@@ -12,21 +12,6 @@ if "6" in marks:
     print("Yes")
 else:
     print("No")
-
-marks = [3, 4, 6 , "Avej",True]
-print(marks)
-print(type(marks))
-print(marks[0])
-print(marks[1])
-print(marks[2])
-print(marks[3])
-print(marks[4])
-print(marks[0])  
-
-if "6" in marks:
-    print("Yes")
-else:
-    print("No")
     
 
 # List Slicing
@@ -43,4 +28,32 @@ print("Length of list:", len(marks))
 
 # Loop through list
 for item in marks:
-    print("Item:", item)                
+    print("Item:", item)  
+
+
+# List Methods
+
+numbers = [10, 20, 30, 40]
+
+print(numbers)
+
+numbers.append(50)
+print("After append:", numbers)
+
+numbers.insert(1, 15)
+print("After insert:", numbers)
+
+numbers.remove(30)
+print("After remove:", numbers)
+
+numbers.sort()
+print("Sorted list:", numbers)
+
+numbers.reverse()
+print("Reverse list:", numbers)
+
+print("Index of 20:", numbers.index(20))
+
+print("Count of 40:", numbers.count(40))
+
+                
