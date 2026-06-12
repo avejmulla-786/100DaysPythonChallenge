7a0cb18919044b2655c52ae4e18cf354ea7c1d11
avej mulla <avejmulla786786@gmail.com>
2026-06-12 22:05:27 +0530

    Day 16 - Advanced Tuple Operations

--- Day17.py
+++ Day17.py
@@ -7,4 +7,13 @@ temp[2] = "Finland"
 countries = tuple(temp)
 print(countries)
 
-tuple = (0,1,2,3,2,3,1,3,2)
+tuple1= (0,1,2,3,2,3,1,3,2)
+res = tuple1.count(3)
+res = tuple1.index(4,8)
+print("Count of 3 in Tuple is:",res)
+print("Length of Tuple:", len(tuple1))
+print("Maximum Value:", max(tuple1))
+print("Minimum Value:", min(tuple1))
+
+for item in tuple1:
+    print(item)
