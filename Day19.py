6943ca44b83e48f1bae9a80651a8f8a2f8f887ac
avej mulla <avejmulla786786@gmail.com>
2026-06-14 23:24:06 +0530

    Day 17 - Advanced String Formatting Practice

--- Day19.py
+++ Day19.py
@@ -3,3 +3,10 @@ country = "india"
 name = "Avej"
 
 print(letter.format(name,country))
+
+# Multiple formatting
+
+age = 20 
+
+message = "My name is {} .I am {} years old and live in {}."
+print(message.format(name,age,country))
