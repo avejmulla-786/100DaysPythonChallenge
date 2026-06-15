54a7e9998440960d149ddd8ca4479d615e7eb646
avej mulla <avejmulla786786@gmail.com>
2026-06-15 22:08:22 +0530

    Day 20 - Docstrings and Temperature Converter

--- Day20.py
+++ Day20.py
@@ -3,3 +3,11 @@ def square(n):
     print(n**2)
 square(5)  
 print(square.__doc__)
+
+#temperature converter
+
+def celsius_to_fahrenheit(c):
+    '''Converts celsius to fahrenheit'''
+    f = (c*9/5)+32
+    print("Temperature in fahrenheit:",f)
+celsius_to_fahrenheit(30)
