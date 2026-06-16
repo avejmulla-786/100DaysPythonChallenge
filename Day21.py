73e184054ab7376df20437ae3993170cf47e5f53
avej mulla <avejmulla786786@gmail.com>
2026-06-16 23:53:33 +0530

    Day 21 - Recursion Practice Programs

--- Day21.py
+++ Day21.py
@@ -6,3 +6,12 @@ def factorial(n):
         return n * factorial(n-1)
     
 print(factorial(5))    
+
+# Sum of Natural Numbers using Recursion
+
+def natural_sum(n):
+    if(n == 0):
+        return 0
+    else:
+        return n + natural_sum(n-1)
+print("sum of natural number ",natural_sum(12))   
