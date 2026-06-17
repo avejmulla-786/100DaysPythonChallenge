302762cc08ddf389784727efbf29ca1fb6db07a7
avej mulla <avejmulla786786@gmail.com>
2026-06-17 20:36:03 +0530

    Day 22 - Set Operations and Membership Check

--- Day22.py
+++ Day22.py
@@ -9,3 +9,28 @@ print(type(Avej))
 
 for value in info:
     print(value)
+
+# Extra Set Operations
+
+numbers = {10,20,30,40,50}
+
+print("Original Set",numbers)
+
+numbers.add(60)
+print("After adding 60",numbers)
+
+numbers.remove(20)
+print("After removing 20",numbers)
+
+print("Length of set:",len(numbers))
+
+if 30 in numbers :
+    print("30 is the present in the set")
+
+else:
+    print("30 is the not present in set")  
+
+#Loop through set 
+for num in numbers:  
+    print("Number:",num)
+      
