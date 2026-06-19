07d32772f1fe87508824a01b3087a9e17aed5369
avej mulla <avejmulla786786@gmail.com>
2026-06-19 23:33:22 +0530

    Day 24 - Advanced Set Operations

--- Day24.py
+++ Day24.py
@@ -4,3 +4,17 @@ print(cities.issuperset(cities2))
 cities3 = {"Tokyo","Madrid","Delhi"}
 print(cities.issuperset(cities3))
 print(cities3.issubset(cities))
+
+
+cities.add("Mumbai")
+print("After Adding Mumbai:", cities)
+
+cities.remove("Berlin")
+print("After Removing Berlin:", cities)
+
+common_cities = cities.intersection(cities3)
+print("Common Cities:", common_cities)
+
+all_cities = cities.union(cities2)
+print("All Cities:", all_cities)
+print("Total Cities:", len(cities))
