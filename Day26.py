38251d4ed2fde77940cca488cb545eb570942d2b
avej mulla <avejmulla786786@gmail.com>
2026-06-21 23:19:27 +0530

    Day 26 - For Loop Else Statement

---
+++ Day26.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i)
+    i = i + 1
+    # if i == 4:
+    #  break 
+
+else:
+    print("Sorry no i")
+
+for x in range(5):
+    print("iterator no{}in for loop".format(x+1))
+else:
+    print("else bolck in loop")
+print("out of the loop")         
