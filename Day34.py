0ce19375ba1e2a62c109aa81b75e26605cc899c9
avej mulla <avejmulla786786@gmail.com>
2026-06-30 10:40:08 +0530

    Day 34 - Advanced Math Module Functions

--- Day34.py
+++ Day34.py
@@ -1,4 +1,4 @@
-from math import sqrt,pi
+from math import sqrt,pi,factorial
 
 result = sqrt(9)*pi
 print(result)
@@ -8,3 +8,17 @@ import math as m
 print(dir(m))
 
 print(m.nan,type(m.nan))
+
+
+# Factorial
+print("Factorial of 5:", factorial(5))
+
+# Power
+print("2 raised to 5 =", m.pow(2, 5))
+
+# Ceiling and Floor
+print("Ceil of 4.2:", m.ceil(4.2))
+print("Floor of 4.8:", m.floor(4.8))
+
+# GCD
+print("GCD of 24 and 36:", m.gcd(24, 36))
