e5d5744121a65b038808a4478cbd3d2482fb8a88
avej mulla <avejmulla786786@gmail.com>
2026-07-13 23:02:58 +0530

    Day 45 - Constructor and Multiple Objects Practice

--- Day45.py
+++ Day45.py
@@ -13,3 +13,20 @@ a = Person("Avej","Devloper")
 a.info()           
 b = Person("harry","HR")
 b.info()
+
+c = Person("Rehan", "Chemical Engineer")
+d = Person("Aman", "Data Analyst")
+
+c.info()
+d.info()
+
+people = [a, b, c, d]
+
+print("\nPerson Details")
+
+for person in people:
+    print(f"Name       : {person.name}")
+    print(f"Occupation : {person.occ}")
+    print("-" * 30)
+
+print("Total Persons:", len(people))
