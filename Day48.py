8a3e016d58904f5967fea4efc61fd272d757f362
avej mulla <avejmulla786786@gmail.com>
2026-07-15 23:36:10 +0530

    Day 47 - Class Method with Multiple Employees

--- Day48.py
+++ Day48.py
@@ -1,15 +1,37 @@
 class Employee:
     company = "Apple"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
     def show(self):
-        print(f"The name is {self.name} and company is {self.company}")
+        print(f"Name    : {self.name}")
+        print(f"Company : {self.company}")
+        print(f"Salary  : ₹{self.salary}")
+        print("-" * 30)
+
+    @classmethod
+    def changeCompany(cls, newCompany):
+        cls.company = newCompany
 
 
-    def changeCompany(self,newCompany):
-        self.company = newCompany
+# Objects
+e1 = Employee("Harry", 50000)
+e2 = Employee("Avej", 70000)
+e3 = Employee("Rehan", 65000)
 
-e1 = Employee()
-e1.name = "Avej"
-e1.show() 
-e1.changeCompany("Tesla")
+print("Before Changing Company")
 e1.show()
-print(Employee.company)           
+e2.show()
+e3.show()
+
+# Change company for all employees
+Employee.changeCompany("Tesla")
+
+print("After Changing Company")
+e1.show()
+e2.show()
+e3.show()
+
+print("Current Company:", Employee.company)
