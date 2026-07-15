0756e1655a0ea7d9b3204a14193cedb742442ecb
avej mulla <avejmulla786786@gmail.com>
2026-07-15 23:32:56 +0530

    Day 47 - Class Methods in Python

---
+++ Day48.py
@@ -0,0 +1,15 @@
+class Employee:
+    company = "Apple"
+    def show(self):
+        print(f"The name is {self.name} and company is {self.company}")
+
+
+    def changeCompany(self,newCompany):
+        self.company = newCompany
+
+e1 = Employee()
+e1.name = "Avej"
+e1.show() 
+e1.changeCompany("Tesla")
+e1.show()
+print(Employee.company)           
