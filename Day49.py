e433332279f756fcb786d13b8da79e94b4ee4375
avej mulla <avejmulla786786@gmail.com>
2026-07-16 15:27:40 +0530

    Day 49 - Alternative Constructor with Multiple Employee Objects

--- Day49.py
+++ Day49.py
@@ -20,3 +20,38 @@ string = "harry-10000"
 e2 = Employee.fromStr(string)
 print(e2.name)
 print(e2.salary)
+print("-" * 30)
+
+# Creating more objects using fromStr()
+
+string1 = "Rehan-25000"
+string2 = "Aman-30000"
+string3 = "Zakir-45000"
+
+e3 = Employee.fromStr(string1)
+e4 = Employee.fromStr(string2)
+e5 = Employee.fromStr(string3)
+
+print(e3.name)
+print(e3.salary)
+
+print(e4.name)
+print(e4.salary)
+
+print(e5.name)
+print(e5.salary)
+
+print("-" * 30)
+
+employees = [e1, e2, e3, e4, e5]
+
+print("Employee Details")
+
+for emp in employees:
+    print(f"Name   : {emp.name}")
+    print(f"Salary : ₹{emp.salary}")
+    print("-" * 20)
+
+print("Total Employees:", len(employees))
+
+
