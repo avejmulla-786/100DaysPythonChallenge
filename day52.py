d66e98c2ff8f9deba8e81602f61b0e8d43c44b01
avej mulla <avejmulla786786@gmail.com>
2026-07-19 16:17:18 +0530

    Day 52 - Shape and Circle Inheritance Practice

--- day52.py
+++ day52.py
@@ -16,3 +16,29 @@ class Circle(Shape):
     
 rec = Shape(3,5)
 print(rec.area())
+
+
+print("-" * 30)
+
+# Creating Circle Object
+circle = Circle(5)
+
+print("Rectangle Area:", rec.area())
+print("Circle Area:", circle.area())
+
+# Creating More Shapes
+rec2 = Shape(10, 4)
+circle2 = Circle(7)
+
+print("-" * 30)
+
+print("Second Rectangle Area:", rec2.area())
+print("Second Circle Area:", circle2.area())
+
+print("-" * 30)
+
+# Display Shape Information
+shapes = [rec, circle, rec2, circle2]
+
+for shape in shapes:
+    print("Area:", shape.area())
