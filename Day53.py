4464944db57c6a7a8c2695b218bc2281afa46c0a
avej mulla <avejmulla786786@gmail.com>
2026-07-20 23:52:10 +0530

    Day 53 - Inheritance and Super Method

---
+++ Day53.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+
+class Programmer(Employee):
+    def __init__(self, name, id, lang):
+        super().__init__(name, id)
+        self.lang = lang
+
+
+rohan = Employee("Rohan Das", "420")
+Avej = Programmer("Avej", "2345", "Python")
+
+print(Avej.name)
+print(Avej.id)
+print(Avej.lang)
