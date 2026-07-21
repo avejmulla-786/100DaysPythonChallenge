5b055c6ad8e63b59402166b52cbb3dd18841d23a
avej mulla <avejmulla786786@gmail.com>
2026-07-21 23:28:45 +0530

    Day 54 - Vector Addition Using Operator Overloading

--- Day54.py
+++ Day54.py
@@ -1,20 +1,42 @@
-#operator overloding
+# Operator Overloading
+
 class vector:
     def __init__(self, i, j, k):
-        self.i = i 
+        self.i = i
         self.j = j
         self.k = k
 
     def __str__(self):
-        return f"{self.i} i + {self.j} j + {self.k} k"
-    
+        return f"{self.i}i + {self.j}j + {self.k}k"
+
     def __add__(self, x):
-        return f"{self.i + x.i}i + {self.j + x.j}j + {self.k + x.k}k"
-    
+        return vector(
+            self.i + x.i,
+            self.j + x.j,
+            self.k + x.k
+        )
+
+
 v1 = vector(3, 5, 6)
 print(v1)
+
 v2 = vector(1, 2, 9)
 print(v2)
 
 print(v1 + v2)
 print(type(v1 + v2))
+
+print("-" * 30)
+
+v4 = vector(10, 20, 30)
+v5 = vector(5, 10, 15)
+
+print("Vector 4:", v4)
+print("Vector 5:", v5)
+
+result = v4 + v5
+
+print("New Vector:", result)
+print("i value:", result.i)
+print("j value:", result.j)
+print("k value:", result.k)
