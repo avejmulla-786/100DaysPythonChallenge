05af70ed50a66bc11c5660bbe0cfc088ac2022aa
avej mulla <avejmulla786786@gmail.com>
2026-07-24 18:18:48 +0530

    Day 57 - Hybrid and Hierarchical Inheritance Practice

--- Day57.py
+++ Day57.py
@@ -28,3 +28,47 @@ obj.show_derived2()
 obj.show_derived3()
 
 print("MRO:", Derived3.mro())
+print("-" * 40)
+
+# Hierarchical Inheritance
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def show_name(self):
+        print(f"Employee Name: {self.name}")
+
+
+class Developer(Employee):
+    def coding(self):
+        print(f"{self.name} works as a Developer")
+
+
+class Designer(Employee):
+    def designing(self):
+        print(f"{self.name} works as a Designer")
+
+
+class Manager(Employee):
+    def managing(self):
+        print(f"{self.name} works as a Manager")
+
+
+dev = Developer("Avej")
+designer = Designer("Aman")
+manager = Manager("Rehan")
+
+dev.show_name()
+dev.coding()
+
+print("-" * 20)
+
+designer.show_name()
+designer.designing()
+
+print("-" * 20)
+
+manager.show_name()
+manager.managing()
+
